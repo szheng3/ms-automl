5f2abcba5b58e4c5576204e4b871dbbe720c7299
Sept <sszheng3@gmail.com>
2023-07-23 17:05:03 -0400

    test

--- python-automl/train_automl.py
+++ python-automl/train_automl.py
@@ -1,37 +1,37 @@
-# import logging
-# import pandas as pd
-# from ludwig.api import LudwigModel
-# from ludwig.datasets import agnews
-# import click
-# import json
-# from sklearn.model_selection import train_test_split
-#
-#
-#
-# class AgnewsModel:
-#
-#     def __init__(self, config, dataset=None, train_df=None, test_df=None, logging_level=logging.INFO):
-#         self.config = config
-#         self.model = LudwigModel(config, logging_level=logging_level)
-#         self.train_df = train_df
-#         self.test_df = test_df
-#         self.train_stats = None
-#         self.test_stats = None
-#         self.predictions = None
-#         self.output_directory = None
-#
-#     def train(self):
-#         self.train_stats, _, self.output_directory = self.model.train(dataset=self.train_df)
-#
-#     def evaluate(self):
-#         self.test_stats, self.predictions, _ = self.model.evaluate(
-#             self.test_df, collect_predictions=True, collect_overall_stats=True
-#         )
-#
-#     def predict(self, text_to_predict):
-#         self.predictions, _ = self.model.predict(text_to_predict)
-#         return self.predictions
-#
+import logging
+import pandas as pd
+from ludwig.api import LudwigModel
+from ludwig.datasets import agnews
+import click
+import json
+from sklearn.model_selection import train_test_split
+
+
+
+class AgnewsModel:
+
+    def __init__(self, config, dataset=None, train_df=None, test_df=None, logging_level=logging.INFO):
+        self.config = config
+        self.model = LudwigModel(config, logging_level=logging_level)
+        self.train_df = train_df
+        self.test_df = test_df
+        self.train_stats = None
+        self.test_stats = None
+        self.predictions = None
+        self.output_directory = None
+
+    def train(self):
+        self.train_stats, _, self.output_directory = self.model.train(dataset=self.train_df)
+
+    def evaluate(self):
+        self.test_stats, self.predictions, _ = self.model.evaluate(
+            self.test_df, collect_predictions=True, collect_overall_stats=True
+        )
+
+    def predict(self, text_to_predict):
+        self.predictions, _ = self.model.predict(text_to_predict)
+        return self.predictions
+
 #
 # @click.command()
 # @click.option(
