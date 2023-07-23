7276b3bde0af0dda1691ba13e6323e7bf8c79b38
Sept <sszheng3@gmail.com>
2023-07-23 17:59:43 -0400

    test

--- python-automl/test_automl.py
+++ python-automl/test_automl.py
@@ -1,8 +1,10 @@
+import torch
 import pandas as pd
-import pytest
-from ludwig.datasets import agnews
+from ludwig.models.inference import InferenceModule
+import json
+import click
 
-from train_automl import AgnewsModel
+from ludwig.utils.inference_utils import to_inference_module_input_from_dataframe
 
 # Test configuration
 test_config = {
@@ -25,10 +27,23 @@ text_to_predict = pd.DataFrame({
 
 
 def test_model_prediction():
-    train_df, test_df, _ = agnews.load(split=True)
+    # train_df, test_df, _ = agnews.load(split=True)
+    #
+    # model = AgnewsModel(test_config, train_df=train_df, test_df=test_df)
+    # model.train()
+    # predictions = model.predict(text_to_predict)
+    # assert predictions is not None
+    # assert len(predictions) == len(text_to_predict)
+    text_to_predict=pd.read_csv("./data/text_to_predict.csv")
 
-    model = AgnewsModel(test_config, train_df=train_df, test_df=test_df)
-    model.train()
-    predictions = model.predict(text_to_predict)
-    assert predictions is not None
-    assert len(predictions) == len(text_to_predict)
+    inference_module = InferenceModule.from_directory('./model/')
+    # output_df = inference_module.predict(text_to_predict)
+    scripted_module = torch.jit.script(inference_module)
+
+    with open(f"./model/model_hyperparameters.json") as f:
+        config = json.load(f)
+
+    input_sample_dict = to_inference_module_input_from_dataframe(text_to_predict, config)
+    # output_df = scripted_module.predict(text_to_predict)
+    output_df = scripted_module(input_sample_dict)
+    assert len(text_to_predict) == len(output_df["class"]["predictions"])
